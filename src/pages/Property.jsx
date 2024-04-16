9419d5e1204bcf0f79c6d2cf3ec42f8ad598b1df
Juwelrana434 <jewel.rana434@gmail.com>
2024-04-16 17:21:09 +0600

    update profile

--- src/pages/Property.jsx
+++ src/pages/Property.jsx
@@ -9,7 +9,7 @@ const Property = ({property}) => {
     return (
         <div>
                <div className="card bg-base-100 shadow-xl">
-  <div className="w-full mb-4"><img src={property.property.image_url} className="8w-full h-[280px] border-2 rounded-md border-gray-100 p-4 shadow-md"/></div>
+  <div className="w-full mb-4"><img src={property.property.image_url} className="w-full h-[280px] border-2 rounded-md border-gray-100 p-4 shadow-md"/></div>
   <div className="">
     <h2 className="card-title my-2 px-4">{property.property.estate_title}</h2>
     <div className="flex justify-between items-center px-4"><p>{property.property.segment_name}</p>
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -162,3 +162,4 @@ const Register = () => {
 };
 
 export default Register;
+
